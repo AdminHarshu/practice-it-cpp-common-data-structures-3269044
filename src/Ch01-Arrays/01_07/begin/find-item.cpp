e0530f898f7bd7f9a5d4317839b13503b021357d
AdminHarshu <104252924+AdminHarshu@users.noreply.github.com>
2025-09-24 07:41:19 +0000

    Done find-item assignment

--- src/Ch01-Arrays/01_07/begin/find-item.cpp
+++ src/Ch01-Arrays/01_07/begin/find-item.cpp
@@ -6,9 +6,29 @@ template <typename T>
 int findItem(const T arr[], size_t size, T target)
 {
     // TODO: implement
+    int result = -1;
+    for (int i = 0; i < size; ++i)
+    {
+        if (arr[i] == target)
+        {
+            result = i;
+            break;
+        }
+    }
+    return result;
 }
 
 int main()
 {
+    int lnNumbers[] = {2,3,6,8,7,1,9,11,23};
+    size_t lnSize = sizeof(lnNumbers) / sizeof(lnNumbers[0]);
+    int lnTarget = 11;
+    int lnResult = findItem( lnNumbers, lnSize, lnTarget);
+    cout << "Find number is :" << lnResult << endl;
+    float lfNumbers[] = { 2.2,6.1,33.3,7.1,99.6};
+    int lfSize = sizeof(lfNumbers) / sizeof(lfNumbers[0]);
+    float lfTarget = 67;
+    int lnResult2 = findItem(lfNumbers, lfSize, lfTarget);
+    cout << "Find number is :" << lnResult2 << endl;
     return 0;
 }
